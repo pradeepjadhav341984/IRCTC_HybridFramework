afb85bd1e3a7213431cc01c9c4ef1dc60738751c
pradeepjadhav341984 <pradeepjadhav3@gmail.com>
2025-07-14 19:25:34 +0530

    increases webdriver wait

--- src/test/java/com/irctc/pages/LoginPage.java
+++ src/test/java/com/irctc/pages/LoginPage.java
@@ -15,7 +15,7 @@ public class LoginPage {
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
     // Elements
